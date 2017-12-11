490e73885c29c7c96e1d14e8d0f700aee097056b
dalazaca <diezucchet@gmail.com>
2017-12-11 12:02:22 -0500

    Fix bug

--- imports/ui/components/Weather.js
+++ imports/ui/components/Weather.js
@@ -1,27 +1,93 @@
 import { Meteor } from 'meteor/meteor';
 import React, { Component, PropTypes } from 'react';
 import { createContainer } from 'meteor/react-meteor-data';
-import Images from '../../api/images/images';
-import Avatar from './Avatar';
 import weather from '../../api/weather/weather';
+
 class Weather extends Component {
     constructor(props) {
         super(props);
 
+        this.state = {
+            descritpion: '',
+            temperature: '',
+            wind: '',
+            precipitation: '',
+        };
+    }
+
+    componentWillReceiveProps(newProps) {
+        if (newProps.ready === true) {
+            this.setState({ email: newProps.currentUser.emails[0].address });
+        }
+    }
+
+    toggleEdit() {
+        this.setState({ editMode: !this.state.editMode });
+    }
+
+    uploadWeather(event) {
+        event.preventDefault();
+
+        if (event.currentTarget.files && event.currentTarget.files[0]) {
+            const file = event.currentTarget.files[0];
+
+                upload.on('end', (error, result) => {
+                    if (error) {
+                        console.log('Error during upload:', error);
+                        this.setState({ imageId: '', fileName: '' });
+                    } else {
+                        Meteor.call('Images.changeAvatar', result._id, (errorAvatar) => {
+                            if (errorAvatar) {
+                                console.log('Error during changeAvatar:', errorAvatar);
+                            } else {
+                                this.setState({ className: 'img-circle img-responsive custom-input-file updated' });
+                            }
+                        });
+                    }
+                });
+
+                upload.start();
+            }
+        }
     }
 
     render() {
+        const {
+            currentUser,
+            ready,
+        } = this.props;
+
+        if (!ready) {
+            return <div>Loading...</div>;
+        }
+
+        const emailLink = `mailto:${currentUser.emails[0].address}`;
+        const mailBlock = this.state.editMode ? editMode : <a href={emailLink}>{this.state.email}</a>;
+
         return (
-            <div className="profile">
-            <h1> Clima en Bogotá: </h1>
-            <h2> Descripcion: </h2> <h3> Broken clouds </h3>
-            <h2> Temperatura: </h2> <h3>12°C </h3> 
-            <h2> Viento: </h2> <h3>2.1 m/s </h3>
-            <h2> Humedad: </h2> <h3>87% </h3>
-            <h2> Precisión: </h2> <h3>1027 hPa </h3>
-            </div>
+                <div className="weather">
+                    <Weather
+                        description={currentUser.weather.description}
+                        wind={currentUser.weather.wind}
+                        temperature={currentUser.weather.temperature}
+                        preciptiation={this.weather.description}
+                        wrapLink={false}
+                    />
+                </div>
         );
     }
 }
 
-export default Weather;
+Profile.propTypes = {
+    currentUser: PropTypes.object,
+    ready: PropTypes.bool,
+};
+
+export default createContainer(() => {
+    const currentUser = Meteor.user();
+
+    return {
+        currentUser,
+        ready: !!currentUser,
+    };
+}, Weather);
